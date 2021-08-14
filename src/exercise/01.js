1b8db12423c5bd2493f25eef37cb6024970e1725
Edwards Moses <edwardsmoses3@gmail.com>
2021-08-14 11:58:07 +0100

    Completed a contrived example of a Device Lockscreen using a useReducer
    https://daveceddia.com/usereducer-hook-examples/

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -8,22 +8,88 @@ function Counter({initialCount = 0, step = 10}) {
   // 💰 React.useReducer(countReducer, initialCount)
 
   const stateReducer = (currentState, action) => {
-    const {type, step} = action
+    const correctPasscode = '4256'
 
-    if (type === 'INCREMENT') {
-      return {...currentState, count: currentState.count + step}
+    const passcode = currentState.typedPasscode + action
+
+    if (passcode.length === correctPasscode.length) {
+      return {
+        status: correctPasscode === passcode ? 'UNLOCKED' : 'LOCKED', //if passcode is correct, unlock the device
+        typedPasscode: '',
+      }
     }
 
-    return currentState
+    return {
+      ...currentState,
+      typedPasscode: passcode,
+    }
   }
 
   const [state, dispatch] = React.useReducer(stateReducer, {
-    count: initialCount,
+    status: 'LOCKED',
+    typedPasscode: '',
   })
-  const {count} = state
-  const increment = () => dispatch({type: 'INCREMENT', step})
 
-  return <button onClick={increment}>{count}</button>
+  const {status, typedPasscode} = state
+
+  const unlock = value => dispatch(value)
+
+  return (
+    <>
+      <h1 style={{textAlign: 'center'}}>{status}</h1>
+      <h3 style={{textAlign: 'center'}}>{typedPasscode}</h3>
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+        <button
+          onClick={() => {
+            unlock(1)
+          }}
+          style={{padding: 10, margin: 10}}
+        >
+          1
+        </button>
+        <button
+          onClick={() => {
+            unlock(2)
+          }}
+          style={{padding: 10, margin: 10}}
+        >
+          2
+        </button>
+        <button
+          onClick={() => {
+            unlock(3)
+          }}
+          style={{padding: 10, margin: 10}}
+        >
+          3
+        </button>
+        <button
+          onClick={() => {
+            unlock(4)
+          }}
+          style={{padding: 10, margin: 10}}
+        >
+          4
+        </button>
+        <button
+          onClick={() => {
+            unlock(5)
+          }}
+          style={{padding: 10, margin: 10}}
+        >
+          5
+        </button>
+        <button
+          onClick={() => {
+            unlock(6)
+          }}
+          style={{padding: 10, margin: 10}}
+        >
+          6
+        </button>
+      </div>
+    </>
+  )
 }
 
 function App() {
